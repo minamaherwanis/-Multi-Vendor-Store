c9b36cf0944940ac46ffedfeb5c67bca9ae4a1bc
Eng: Mina <minamaherwanis@gmail.com>
2025-07-16 10:48:35 +0300

    Middleware (Check Last Active Time)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,15 +6,19 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'checkUserType' => \App\Http\Middleware\CheckUserType::class,
+        ]);
 
-          $middleware->alias([
-        'checkUserType' => \App\Http\Middleware\CheckUserType::class,
-    ]);
+        // ✅ ميدل وير بيشتغل فقط على web.php
+        $middleware->web([
+            \App\Http\Middleware\UpdateUserLastActiveAt::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
