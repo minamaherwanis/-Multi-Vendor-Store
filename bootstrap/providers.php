6f0e61d5eb46fa2e0b58e10a516765214bcb1288
Eng: Mina <minamaherwanis@gmail.com>
2025-09-18 17:40:46 +0300

    Laravel Fortify Installation and Configuration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,7 +4,8 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\CartServiceProvider::class,
     App\Providers\BroadcastServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
 
-   // App\Providers\EventServiceProvider::class,
-  //معموله كومنت لان ف لارافيل 11 وفوق بقي بعمل اكتنشاف تلقائي للحدث والمستمع فلو سبناه هينفز الكود مرتين
- ];
+    // App\Providers\EventServiceProvider::class,
+    //معموله كومنت لان ف لارافيل 11 وفوق بقي بعمل اكتنشاف تلقائي للحدث والمستمع فلو سبناه هينفز الكود مرتين
+];
